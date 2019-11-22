08747129bbf09f099b97db498dab51ddac30fb5c
lioness3 <jcnailtech3@yahoo.com>
2019-11-22 11:07:25 -0800

    correct API with lat and long

--- src/business.js
+++ src/business.js
@@ -1,7 +1,7 @@
 export class DoctorName{
   async getName() {
     try {
-      let nameResponse = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=oregon-portland&user_key=${process.env.API_KEY}`);
+      let nameResponse = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userSymptom}&user_location=45.5051%2C122.6750&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResponse = await nameResponse.json();
       return jsonifiedResponse;
     } catch(error) {
--- src/user.js
+++ src/user.js
@@ -3,3 +3,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function(){
+  $("#symptomButton").click(function(event){
+    event.preventDefault;
+    const userSymptom = $("#symptom").val();
+    $("#symptom").val('');
+    (async () => {
+              let doctor = new DoctorName();
+              const nameResponse = await docotor.getName();
+              getPath(nameResponse);
+            })();
+            function getPath(nameResponse) {
+            $("#showDoctor").text(`Here is a list of doctors in the Portland area: ${nameResponse.}`)
+  })
+})
