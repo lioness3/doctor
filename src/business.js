95baaf7bb18461621af4bc30ccdb198e719378c6
lioness3 <jcnailtech3@yahoo.com>
2019-11-23 19:29:13 -0800

    add catch error

--- src/business.js
+++ src/business.js
@@ -8,13 +8,19 @@ export class DoctorName{
   //   alert("hi");
   //   }
   // }
+
   async getName(userSymptom) {
     try {
       let nameResponse = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userSymptom}&user_location=45.5051%2C122.6750&skip=0&limit=10&user_key=262c57b8c7d02a5e7fab8baba2a526b4`);
-      let jsonifiedResponse = await nameResponse.json();
-      return jsonifiedResponse;
-    } catch(error) {
-    alert("hi");
+        let jsonifiedResponse = await nameResponse.json();
+        return jsonifiedResponse;
+      } catch(error) {
+        if (this.status === 200) {
+          resolve(nameResponse.response);
+        } else {
+          reject(Error(nameResponse.statusText));
+        }
+
+      }
     }
   }
-}
--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 // If the query response includes any doctors, the following information should be included about each doctor: first name, last name, address, phone number, website and whether or not the doctor is accepting new patients (the API provides this data).
-// If the API call results in an error (any message not a 200 OK), the application should return a notification that states what the error is.
 // If the query response doesn't include any doctors (for instance, if no doctors meet the search criteria), the application should return a notification that states that no doctors meet the criteria. (This is not an error so it should be handled separately from any errors.)
 
 $(document).ready(function(){
@@ -16,24 +15,27 @@ $(document).ready(function(){
     // $("#name").val('');
     $("#symptom").val('');
     (async () => {
-              let doctor = new DoctorName();
-              const nameResponse = await doctor.getName(userSymptom);
-              // const nameResponse = await doctor.getName(docName, userSymptom);
-              getPath(nameResponse);
-            })();
-            function getPath(nameResponse) {
-              if(this.value=== true){
-              // let arr = [];
-              // for(var i = 0; i < nameResponse.length; i ++){
-              //   arr.push(`${nameResponse.data[0].practices[i].name}`);
-              // }
-              // $("#showDoctor").text(`Here is a list of doctors in the Portland area:
-              //    `);
-                 console.log(`${nameResponse.data[0].practices[0]}`);
+      let doctor = new DoctorName();
+      const nameResponse = await doctor.getName(userSymptom);
+      // const nameResponse = await doctor.getName(docName, userSymptom);
+      getPath(nameResponse);
+    })();
+    function getPath(nameResponse) {
+      if(this.value === true){
+        // let arr = [];
+        // for(var i = 0; i < nameResponse.length; i ++){
+          //   arr.push(`${nameResponse.data[0].practices[i].name}`);
+          // }
+          // $("#showDoctor").text(`Here is a list of doctors in the Portland area:
+            //    `);
+            console.log(`${nameResponse.data[0].practices[0]}`);
 
           }else {
             $("#showDoctor").text('No doctors meet this criteria.')
           }
+
         }
+      }
+    });
   });
-});
+// If the API call results in an error (any message not a 200 OK), the application should return a notification that states what the error is.
