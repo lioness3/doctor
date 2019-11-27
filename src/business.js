80030c976ab184dec3c4b7fbefa68122e846916d
lioness3 <jcnailtech3@yahoo.com>
2019-11-27 15:26:49 -0800

    catch 400 error

--- src/business.js
+++ src/business.js
@@ -1,25 +1,30 @@
 export class DoctorName{
   async getName(docName, userSymptom) {
-    try {
-  let nameResponse = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&query=${userSymptom}&user_location=45.5051%2C122.6750&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-      let jsonifiedResponse = await nameResponse.json();
-      return jsonifiedResponse;
-    } catch(error) {
-    alert("hi");
-    }
-  }
+      try {
+      let nameResponse = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&query=${userSymptom}&user_location=45.5051%2C122.6750&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+            let jsonifiedResponse = await nameResponse.json();
+            return jsonifiedResponse;
+          } catch(error) {
 
-  // async getName(userSymptom) {
-  //   try {
-  //     let nameResponse = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${userSymptom}&user_location=45.5051%2C122.6750&skip=0&limit=10&user_key=262c57b8c7d02a5e7fab8baba2a526b4`);
-  //       let jsonifiedResponse = await nameResponse.json();
-  //       return jsonifiedResponse;
-  //     } catch(error) {
-  //       if (this.status === 200) {
-  //         resolve(nameResponse.response);
-  //       } else {
-  //         reject(Error(nameResponse.statusText));
-  //       }
-  //
-  //     }
-    }
+              console.error("There was an error handling your request: " + error.message);
+
+            }
+            }
+        
+//
+//           async getName(docName,userSymptom) {
+//             try {
+//               let nameResponse = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&query=${userSymptom}&user_location=45.5051%2C122.6750&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+//                 let jsonifiedResponse = await nameResponse.json();
+//                 return jsonifiedResponse;
+//               } catch(error) {
+//                 if (this.status === 200) {
+//                   resolve(nameResponse.response);
+//                 } else {
+//                   reject(Error(nameResponse.statusText));
+//                 }
+//
+//
+//               }
+//             }
+}
